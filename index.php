ac6909c879c5ed2c82c54c39731693955b42599a
luoyouxiu <1291838675@qq.com>
2024-02-04 17:30:02 +0800

    模板示例

--- index.php
+++ index.php
@@ -7,9 +7,13 @@ use LuoYouXiu\FadadaSdkForPhp\Constants\OpenApiConfigConstants;
 
 require "vendor/autoload.php";
 
-
+#实例化服务对象
 $getService = new GetService();
+#获取token
 $accessToken = $getService->getAccessToken();
-$euiClient = new EUIClient(new Client(OpenApiConfigConstants::APP_ID, OpenApiConfigConstants::APP_SECRET, OpenApiConfigConstants::SERVICE_URL));
-
+#实例化基础参数对象
+$OpenApiConfigConstants = new OpenApiConfigConstants('app_id', 'app_secret', 'service_url');
+#实例化客户端对象
+$euiClient = new EUIClient(new Client($OpenApiConfigConstants->APP_ID,$OpenApiConfigConstants->APP_SECRET, $OpenApiConfigConstants->SERVICE_URL));
+#其他操作
 var_dump($accessToken);
--- src/constants/OpenApiConfigConstants.php
+++ src/constants/OpenApiConfigConstants.php
@@ -4,7 +4,18 @@ namespace LuoYouXiu\FadadaSdkForPhp\Constants;
 
 class OpenApiConfigConstants
 {
-
+    /**
+     *企业主体在应用内的的clientUserId
+     */
+    const clientUserId = "";
+    /**
+     *重定向地址
+     */
+    const RedirectUrl = "";
+    /**
+     *自动签署免签场景码
+     */
+    const BusinessId = "";
     /**
      * 接口请求域名
      *
@@ -13,52 +24,30 @@ class OpenApiConfigConstants
      *
      */
     public $SERVICE_URL = "";
-
     /**
      * 应用appid
      */
     public $APP_ID = "";
-
-
     /**
      * 应用秘钥
      */
     public $APP_SECRET = "";
-
-
     /**
      *企业主体在应用上的openCorpId
      */
     public $OpenCorpId = "";
 
     /**
-     *企业主体在应用内的的clientUserId
-     */
-    const clientUserId  = "";
-
-
-    /**
-     *重定向地址
-     */
-    const RedirectUrl  = "";
-
-
-    /**
-     *自动签署免签场景码
-     */
-    const BusinessId = "";
-
-
-    /**
      * @param string $APP_ID
      * @param string $APP_SECRET
      * @param string $SERVICE_URL
      */
-    public function __construct(string $APP_ID,string $APP_SECRET,string $SERVICE_URL)
+    public function __construct(string $APP_ID, string $APP_SECRET, string $SERVICE_URL)
     {
         $this->APP_ID = $APP_ID;
         $this->APP_SECRET = $APP_SECRET;
         $this->SERVICE_URL = $SERVICE_URL;
     }
+
 
 }
